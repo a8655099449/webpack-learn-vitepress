f6818c3b0d9f0323918257a783efa8ed680b22e5
大步佬 <865509949@qq.com>
2022-07-04 16:13:17 +0800

    2022年7月4日16:13:16

--- docs/.vitepress/side/learn.ts
+++ docs/.vitepress/side/learn.ts
@@ -6,6 +6,8 @@ const learnSide = [
       { text: "webpack的初体验", link: "/pages/learn/webpack/" },
       { text: "webpack中的js资源", link: "/pages/learn/webpack/01" },
       { text: "开发服务器", link: "/pages/learn/webpack/02" },
+      { text: "webpack中的高级配置", link: "/pages/learn/webpack/03" },
+      { text: "webpack中常见的面试题", link: "/pages/interview/webpack" },
     ],
   },
   {
@@ -13,9 +15,13 @@ const learnSide = [
     collapsible: true,
     items: [
       { text: "js 中的 set 、 map 、weakMap 、 weakSet", link: "/pages/learn/js/set" },
-      { text: "webpack中常见的面试题", link: "/pages/learn/webpack/interview" },
     ],
   },
 ];
+
+// class A {
+//   constructor
+// }
+
 
 export default learnSide;
