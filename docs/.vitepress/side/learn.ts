2887c41ee8245f567180051fd2a26bca4f08d5e0
大步佬 <865509949@qq.com>
2022-07-11 18:39:27 +0800

    2022年7月11日18:39:26

--- docs/.vitepress/side/learn.ts
+++ docs/.vitepress/side/learn.ts
@@ -1,21 +1,31 @@
 const learnSide = [
-  {
-    text: "webpack",
-    collapsible: true,
-    items: [
-      { text: "webpack的初体验", link: "/pages/learn/webpack/" },
-      { text: "webpack中的js资源", link: "/pages/learn/webpack/01" },
-      { text: "开发服务器", link: "/pages/learn/webpack/02" },
-    ],
-  },
-  {
-    text: "js基础",
-    collapsible: true,
-    items: [
-      { text: "js 中的 set 、 map 、weakMap 、 weakSet", link: "/pages/learn/js/set" },
-      { text: "webpack中常见的面试题", link: "/pages/learn/webpack/interview" },
-    ],
-  },
-];
+	{
+		text: 'webpack',
+		collapsible: true,
+		items: [
+			{ text: 'webpack的初体验', link: '/pages/learn/webpack/' },
+			{ text: 'webpack中的js资源', link: '/pages/learn/webpack/01' },
+			{ text: '开发服务器', link: '/pages/learn/webpack/02' },
+		],
+	},
+	{
+		text: 'js基础',
+		collapsible: true,
+		items: [
+			{
+				text: 'js 中的 set 、 map 、weakMap 、 weakSet',
+				link: '/pages/learn/js/set',
+			},
+			{ text: 'webpack中常见的面试题', link: '/pages/learn/webpack/interview' },
+		],
+	},
+	{
+		text: '小程序开发',
+		collapsible: true,
+		items: [
+			{ text: '小程序页面和组件的创建', link: '/pages/learn/miniapp/01' },
+		],
+	},
+]
 
-export default learnSide;
+export default learnSide
